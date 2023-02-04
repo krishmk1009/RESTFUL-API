07b1e4b0f7a9eb32f76a39da10b93cec89d716a1
krishmk1009 <krushnamk1009@gmail.com>
2023-02-04 16:39:01 +0530

    post_deleted commited

--- app.js
+++ app.js
@@ -43,6 +43,35 @@ app.get('/articles', (req, res) => {
         }
     })
 })
+
+app.post('/articles', (req, res) => {
+    // let title = req.body.title;
+    // let content = req.body.content;
+    // res.send(title)
+    // res.send(content);
+
+    var article1 = new Article({ title: req.body.title, content: req.body.content });
+
+    article1.save((err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send("succesfully added the document")
+        }
+    })
+})
+
+app.delete('/articles', (req, res) => {
+    Article.deleteMany({}, (err) => {
+        if (!err) {
+            console.log("deleted succesfully");
+            res.send("deleted succcesfully")
+
+        }
+        console.log(err);
+    })
+})
 app.listen(3000, () => {
     console.log("server started on port 3000")
 })
